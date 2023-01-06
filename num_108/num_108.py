0cfbb72947bdc750f9a7ecf6704696af390edc46
Vera <solnce288888@gmail.com>
2023-01-06 19:22:48 +0300

    num_108

--- num_108/num_108.py
+++ num_108/num_108.py
@@ -4,3 +4,12 @@
 # метод Распаковка: на вход подается сжатый текстовый файл, на выходе текстовый файл восстановленный.
 # Прикинуть достигаемую степень сжатия (отношение количества байт сжатого к исходному).
 
+from rle import rle_encoding as enc, rle_decodoing as dec
+enc('num_108\\file_1.txt')
+dec('num_108\\file_1_rle_enc.txt')
+import os
+inputstats = os.stat('num_108\\file_1.txt')
+outstats = os.stat('num_108\\file_1_rle_enc.txt')
+inputsize = inputstats.st_size
+outputsize = outstats.st_size
+print('Степень сжатия', round(outputsize / inputsize, 3)) 
--- num_108/rle.py
+++ num_108/rle.py
@@ -1,17 +1,33 @@
 def rle_encoding(File_name):
     with open(File_name, 'r', encoding='UTF-8') as file, \
-    open(File_name[:-4]+'_rle_enc.txt','w+', encoding='UTF-8') as out:
+    open(File_name[:-4]+'_rle_enc.txt','w', encoding='UTF-8') as out:
         for item in file.readlines():
             temp = item.rstrip()
             res = ''
+            count = 1
             for i in range(1, len(temp)):
-                count = 1
-                if temp[i - 1].isalpha():
-                    while temp[i - 1] == temp[i]:
-                        count += 1
-                    res += str(count) + temp[i-1]
+                if temp[i] == temp[i - 1]:
+                    count += 1
                 else:
-                    res += temp[i-1]
-            print(res, file=out, end='\n')
+                    res += str(count) + temp[i - 1]
+                    count = 1
+                if i == len(temp) - 1:
+                    res += str(count) + temp[i - 1]               
+            print(res, file=out)
 
-rle_encoding("file_1.txt")
+def rle_decodoing(file_name):
+    with open(file_name, 'r', encoding='UTF-8') as f, \
+    open(file_name[:-4]+'_rle_dec.txt','w', encoding='UTF-8') as out:
+        for item in f.readlines():
+            temp = item.rstrip()
+            res = ''
+            count = ''
+            for i in range(1, len(temp) + 1):
+                if temp[i - 1].isdigit() and temp[i].isalpha():
+                    count += temp[i - 1]
+                    res += temp[i]*int(count)
+                    count = ''
+                elif temp[i - 1].isdigit() and temp[i].isdigit():
+                    count += temp[i - 1]
+               
+            print(res, file=out)
